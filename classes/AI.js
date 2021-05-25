20ef275674a84137640b9a97d7a2771dd7cbafc0
DCarr40 <83989305+DCarr40@users.noreply.github.com>
2021-05-24 22:25:20 -0400

    added chooseGesture function to Human.js and AI.js

--- classes/AI.js
+++ classes/AI.js
@@ -1,103 +1,28 @@
 "use strict"
 
-const Player = require ('./Player');
+const Player = require('./Player');
 
 class AI extends Player {
+
+    constructor(name, score){
+        super(name, score);
+        this.AiChoice = aiChoice;
+    }
 
 
 
     chooseGesture() {
         let aiChoice = Math.random();
 
-        if (aiChoice < 0.2) {
-            aiChoice = "rock";
-        } else if (aiChoice <= 0.4) {
-            aiChoice = "paper";
-        } else if (aiChoice <= 0.6) {
-            aiChoice = "scissors";
-        } else if (aiChoice <= 0.8) {
-            aiChoice = "lizard";
-        } else {
-            aiChoice = "spock";
-        }
-        alert("The computer chose " + aiChoice);
-        var compare = function (choice1, choice2) {
-            if (choice1 === choice2) {
-                alert("And... It's a tie!");
-            }
-            //If the user chose rock...
-            else if (choice1 === "rock") {
-                if (choice2 === "scissors") {
-                    alert("Rock wins!");
-                } else if (choice2 === "paper") {
-                    alert("Paper wins!");
-                } else if (choice2 === "lizard") {
-                    alert("Rock wins!");
-                } else {
-                    alert("Spock wins!");
-                }
-            }
-            //If the user chose paper...
-            else if (choice1 === "paper") {
-                if (choice2 === "scissors") {
-                    alert("Scissors wins!");
-                } else if (choice2 === "rock") {
-                    alert("Paper wins!");
-                } else if (choice2 === "lizard") {
-                    alert("Lizard wins!");
-                } else {
-                    alert("Paper wins!");
-                }
-            }
-            //If the user chose scissors...
-            else if (choice1 === "scissors") {
-                if (choice2 === "paper") {
-                    alert("Scissors wins!");
-                } else if (choice2 === "rock") {
-                    alert("Rock wins!");
-                } else if (choice2 === "lizard") {
-                    alert("Scissors wins!");
-                } else {
-                    alert("Spock wins!");
-                }
-            }
-            //If the user chose lizard...
-            else if (choice1 === "lizard") {
-                if (choice2 === "scissors") {
-                    alert("Scissors wins!");
-                } else if (choice2 === "rock") {
-                    alert("Rock wins!");
-                } else if (choice2 === "paper") {
-                    alert("Lizard wins!");
-                } else {
-                    alert("Lizard wins!");
-                }
-            }
-            //If the user chose spock...
-            else if (choice1 === "spock") {
-                if (choice2 === "scissors") {
-                    alert("Spock wins!");
-                } else if (choice2 === "rock") {
-                    alert("Spock wins!");
-                } else if (choice2 === "lizard") {
-                    alert("Lizard wins!");
-                } else {
-                    alert("Paper wins!");
-                }
-            }
-        };
-        compare(userChoice, aiChoice);
-        Collapse
+        if (aiChoice < 0.2) { aiChoice = "rock"; }
+        else if (aiChoice <= 0.4) { aiChoice = "paper"; }
+        else if (aiChoice <= 0.6) { aiChoice = "scissors"; }
+        else if (aiChoice <= 0.8) { aiChoice = "lizard"; }
+        else { aiChoice = "spock"; }
 
-
-
-
-
-
-
-
-
-
-        return aiChoice;
+        console.log("The computer chose " + aiChoice);
+        return this.AiChoice = aiChoice;
     }
+
+    
 }
